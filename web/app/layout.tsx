e89f51541fd2d11653bf2786f868df0fe12ee5af
cryptoflops <cryptoflops@users.noreply.github.com>
2026-05-26 22:03:50 +0300

    feat: add Talent Protocol project verification meta tag

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Jackpot Wall | Stacks",
   description: "Post to the wall, trigger the jackpot.",
+  other: {
+    "talentapp:project_verification": "4ab7e8289f1784078cb14cfc1735d2531fdde6aef6fbf1c6beac499a6be8ac144586d80a51528dd84b64f5400b15203ee4e0eabc037ae3ed8e427e1d9bb69801"
+  }
 };
 
 export default function RootLayout({
